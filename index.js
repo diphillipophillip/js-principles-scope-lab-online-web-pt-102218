a96a0a46909c4548edef3c5083a1ae9998e5ba00
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-16 14:27:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,9 @@
 function overwriteBestCustomer() {
   bestCustomer = 'maybe bob';
 }
+
+const leastFavoriteCustomer = 'me'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'you';
+}
